a27cf746636685f84f6603900a5a0ac68cdac114
Chuanwang Wu <chuanwangwu9@gmail.com>
2024-11-14 00:33:15 +0800

    end firebase

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,24 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const yahooFinance = require("yahoo-finance2").default;
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.helloWorld = onRequest(async (request, response) => {
+  try {
+    // Perform the search for AAPL stock
+    const results = await yahooFinance.search("AAPL");
+
+    // Log the results for debugging purposes
+    logger.info("Search results: ", {structuredData: true, results});
+
+    // Send the results back as the response
+    response.json(results);
+  } catch (error) {
+    // If there's an error, log it and send an error message
+    logger.error("Error occurred while fetching Yahoo Finance data", error);
+    response.status(500).send("Error fetching data from Yahoo Finance");
+  }
 });
