5b69cb478bdb01013b7ede433c7e6beb1263373a
Chuanwang Wu <chuanwangwu9@gmail.com>
2024-11-15 21:45:08 +0800

    base get yahoo finance

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,26 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
+const yahooFinance = require("yahoo-finance2").default;
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase! new 2");
+exports.helloWorld = onRequest(async (request, response) => {
+  try {
+    // 查询AAPL的相关信息
+    const results = await yahooFinance.search("AAPL");
+
+    // 记录日志
+    logger.info("Yahoo Finance Results", {structuredData: true});
+
+    // 返回 JSON 格式的响应
+    response.status(200).json(results);
+  } catch (error) {
+    // 如果发生错误，返回错误信息
+    logger.error("Error fetching Yahoo Finance data", {structuredData: true});
+    response
+        .status(500)
+        .json({error: "Failed to fetch data from Yahoo Finance"});
+  }
 });
